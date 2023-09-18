da82815ed0d298d139cd446cc65ae90e812e04a0
tarako25 <takenoko1603@gmail.com>
2023-09-18 16:08:01 +0900

    post画面にIDと名前表示

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,64 +1,19 @@
-"use client"
-import React, { useState, useEffect } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { getPageSession } from "@/auth/lucia";
+import { redirect } from "next/navigation";
+import Post from "@/components/post";
+import Form from "@/components/form";
 
-const page = () => {
+const Page = async () => {
+	const session = await getPageSession();
+	if (!session) redirect("/login");
+	return (
+		<>
+      <Post userId={session.user.userId} username={session.user.username}/>
+			<Form action="/api/logout">
+				<input type="submit" value="Sign out" />
+			</Form>
+		</>
+	);
+};
 
-
-  /*
-  const searchParams = useSearchParams();
-  const a = searchParams.get("no")
-  console.log(a);
-  */
-  interface Data {
-    user: {
-        id: string;
-        username: string;
-    };
-      id: number;
-      content: string;
-      userId: string;
-  }
-  const [data, setData] = useState<Data | null>(null);
-
-  useEffect(() =>{
-    const feychLoard = async () =>{
-      const currentURL = window.location.href;
-      const url = new URL(currentURL);
-      const post_no = url.searchParams.get("no");
-
-      //api(GET)
-      const response = await fetch('http://localhost:3000/api/post', {
-            method: "POST",
-            headers: {
-              'Content-type':'application/json',
-            },
-            body: JSON.stringify(post_no)
-          });
-          if(!response.ok){
-            console.log("ロード中にエラーが発生しました");
-        }
-        const element = await response.json();
-        if(data === null) {
-          setData(element.post);
-        }
-      }
-      feychLoard()
-  },[])
-  console.log(data)
-
-  return (
-    <>
-    <div>
-      <p>
-      {data ? data.user.username : null}
-      </p>
-      <p>
-        {data ? data.content : null}
-      </p>
-    </div>
-    </>
-  )
-}
-
-export default page
+export default Page;
