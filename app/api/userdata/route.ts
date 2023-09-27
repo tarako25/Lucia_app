1d98e60a01a8c5ac58c6167ee4d744e3f442f69b
tarako25 <takenoko1603@gmail.com>
2023-09-27 22:03:50 +0900

    userにproductionカラムを追加

---
+++ app/api/userdata/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import  prisma  from "@/lib/prisma";
+
+export async function DB(){
+    try {
+        await prisma.$connect();
+    } catch (error) {
+        return Error("DB接続に失敗しました")
+    }
+}
+
+export async function POST (req: NextRequest, res:NextResponse){
+    try {
+        await DB();
+        const userId = await req.json()
+        
+        const user = await prisma.user.findFirst({
+            where: {
+                id: userId
+            },
+        });
+
+        return NextResponse.json({ message: "Success", user}, {status: 201});
+    } catch (err) {
+        console.log("エラー",err)
+        return NextResponse.json({ message: "Error", err}, {status: 500});
+    } finally {
+        await prisma.$disconnect();
+    }
+}
--- components/profile.tsx
+++ components/profile.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 
 function profile(props:any) {
 
     const {userId, username} = props;
 
+    interface Item{
+        username: string,
+        production: string
+    }
+    const [userData, setUserData] = useState<Item>();
+
+    const getUserData = async() => {
+        const Id = userId
+        const response = await fetch('http://localhost:3000/api/userdata',{
+            method: "POST",
+            headers: {
+            'Content-type':'application/json',
+        },
+            body:JSON.stringify(Id)
+        });
+        if(!response.ok){
+            console.log("ロード中にエラーが発生しました");
+        }
+        const data = await response.json();
+        setUserData(data.user)
+        console.log(data.user)
+    }
+    useEffect(() => {
+        getUserData()
+    },[])
   return (
     <div className='flex flex-col items-center'>
         <div className='w-full mt-5 px-4 py-5 rounded bg-white border'>
@@ -11,13 +37,13 @@ function profile(props:any) {
                 img
             </div>
             <div className='flex justify-between mb-5'>
-                <div>名前</div>
+                <div>{userData?.username}</div>
                 <div>アカウント情報を編集</div>
             </div>
             <div className='flex flex-col items-start'>
-                <div className='mb-5'>よろしくお願いします</div>
+                <div className='mb-5'>{userData?.production}</div>
                 <div className='flex'>
-                    <div>2フォロー</div>
+                    <div className='mr-2'>2フォロー</div>
                     <div>2フォロワー</div>
                 </div>
             </div>
